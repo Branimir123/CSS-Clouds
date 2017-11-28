872fba17f7b48890af445477851999b4e23d4f7a
Branimir123 <brani_svetoslavov@abv.bg>
2017-11-28 13:58:05 +0200

    Add cloud image

--- js/clouds.js
+++ js/clouds.js
@@ -1,57 +1,117 @@
-/*
-  Defining our variables
-  world and viewport are DOM elements,
-  worldXAngle and worldYAngle are floats that hold the world rotations,
-  d is an int that defines the distance of the world from the camera
-*/
-var world = document.getElementById( 'world' ),
-viewport = document.getElementById( 'viewport' ),
-worldXAngle = 0,
-worldYAngle = 0,
-d = 0;
+(function() {
+  var lastTime = 0;
+  var vendors = ['ms', 'moz', 'webkit', 'o'];
+  for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+    window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+    window.cancelRequestAnimationFrame = window[vendors[x]+
+      'CancelRequestAnimationFrame'];
+  }
 
-/*
-Event listener to transform mouse position into angles
-from -180 to 180 degress, both vertically and horizontally
-*/
-window.addEventListener( 'mousemove', function( e ) {
+  if (!window.requestAnimationFrame)
+    window.requestAnimationFrame = function(callback, element) {
+      var currTime = new Date().getTime();
+      var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+      var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+        timeToCall);
+      lastTime = currTime + timeToCall;
+      return id;
+    };
 
-worldYAngle = -( .5 - ( e.clientX / window.innerWidth ) ) * 180;
-worldXAngle = ( .5 - ( e.clientY / window.innerHeight ) ) * 180;
-updateView();
+  if (!window.cancelAnimationFrame)
+    window.cancelAnimationFrame = function(id) {
+      clearTimeout(id);
+    };
+}())
 
-} );
+var layers = [],
+  objects = [],
+
+  world = document.getElementById( 'world' ),
+  viewport = document.getElementById( 'viewport' ),
+
+  d = 0,
+  p = 400,
+  worldXAngle = 0,
+  worldYAngle = 0;
+
+viewport.style.webkitPerspective = p;
+viewport.style.MozPerspective = p;
+viewport.style.oPerspective = p;
 
 generate();
 
-/*
-Changes the transform property of world to be
-translated in the Z axis by d pixels,
-rotated in the X axis by worldXAngle degrees and
-rotated in the Y axis by worldYAngle degrees.
-*/
-function updateView() {
+function createCloud() {
 
-world.style.transform = 'translateZ( ' + d + 'px ) \
-rotateX( ' + worldXAngle + 'deg) \
-rotateY( ' + worldYAngle + 'deg)';
+  var div = document.createElement( 'div'  );
+  div.className = 'cloudBase';
+  var x = 256 - ( Math.random() * 512 );
+  var y = 256 - ( Math.random() * 512 );
+  var z = 256 - ( Math.random() * 512 );
+  var t = 'translateX( ' + x + 'px ) translateY( ' + y + 'px ) translateZ( ' + z + 'px )';
+  div.style.webkitTransform = t;
+  div.style.MozTransform = t;
+  div.style.oTransform = t;
+  world.appendChild( div );
+
+  for( var j = 0; j < 5 + Math.round( Math.random() * 30 ); j++ ) {
+    var cloud = document.createElement( 'img' );
+    cloud.className = 'cloudLayer';
+
+    var x = 256 - ( Math.random() * 512 );
+    var y = 256 - ( Math.random() * 512 );
+    var z = 100 - ( Math.random() * 200 );
+    var a = Math.random() * 360;
+    var s = .25 + Math.random();
+    x *= .2; y *= .2;
+    cloud.data = {
+      x: x,
+      y: y,
+      z: z,
+      a: a,
+      s: s,
+      speed: .1 * Math.random()
+    };
+    var t = 'translateX( ' + x + 'px ) translateY( ' + y + 'px ) translateZ( ' + z + 'px ) rotateZ( ' + a + 'deg ) scale( ' + s + ' )';
+    cloud.style.webkitTransform = t;
+    cloud.style.MozTransform = t;
+    cloud.style.oTransform = t;
+
+    div.appendChild( cloud );
+    layers.push( cloud );
+  }
+
+  return div;
+}
+
+window.addEventListener( 'mousewheel', onContainerMouseWheel );
+window.addEventListener( 'DOMMouseScroll', onContainerMouseWheel );
+window.addEventListener( 'mousemove', onMouseMove );
+window.addEventListener( 'touchmove', onMouseMove );
+
+function onMouseMove ( e ) {
+
+  var x = e.clientX || e.touches[ 0 ].clientX;
+  var y = e.clientY || e.touches[ 0 ].clientY;
+
+  worldYAngle = -( .5 - ( x / window.innerWidth ) ) * 180;
+  worldXAngle = ( .5 - ( y / window.innerHeight ) ) * 180;
+  updateView();
+  event.preventDefault();
 
 }
-/*
-  objects is an array of cloud bases
-  layers is an array of cloud layers
-*/
-var objects = [],
-layers = [];
 
-/*
-Clears the DOM of previous clouds bases
-and generates a new set of cloud bases
-*/
+function onContainerMouseWheel( event ) {
+
+  event = event ? event : window.event;
+  d = d - ( event.detail ? event.detail * -5 : event.wheelDelta / 8 );
+  updateView();
+  event.preventDefault();
+
+}
+
 function generate() {
 
   objects = [];
-  layers = [];
 
   if ( world.hasChildNodes() ) {
     while ( world.childNodes.length >= 1 ) {
@@ -59,50 +119,34 @@ function generate() {
     }
   }
 
-  for( var j = 0; j < 1; j++ ) {
+  for( var j = 0; j < 5; j++ ) {
     objects.push( createCloud() );
   }
 
 }
 
-/*
-Creates a single cloud base: a div in world
-that is translated randomly into world space.
-Each axis goes from -256 to 256 pixels.
-*/
-function createCloud() {
+function updateView() {
 
-  var div = document.createElement( 'div'  );
-  div.className = 'cloudBase';
-  var t = 'translateX( ' + Math.random() * 10 + 'px ) \
-    translateY( ' + Math.random() * 300 + 'px ) \
-    translateZ( ' + Math.random() * 300 + 'px )';
-  div.style.transform = t;
-  world.appendChild( div );
-
-  for( var j = 0; j < 5 + Math.round( Math.random() * 10 ); j++ ) {
-    var cloud = document.createElement( 'div' );
-    cloud.className = 'cloudLayer';
-
-    cloud.data = {
-      x: Math.random() * 100,
-      y: Math.random() * 100,
-      z: Math.random() * 100,
-      a: Math.random() * 100,
-      s: Math.random() * 1
-    };
-
-    var t = 'translateX( ' + cloud.data.x + 'px ) \
-      translateY( ' + cloud.data.y + 'px ) \
-      translateZ( ' + cloud.data.z  + 'px ) \
-      rotateZ( ' + cloud.data.a + 'deg ) \
-      scale( ' + cloud.data.s  + ' )';
-    cloud.style.transform = t;
-
-    div.appendChild( cloud );
-    layers.push( cloud );
-  }
-
-  return div;
+  var t = 'translateZ( ' + d + 'px ) rotateX( ' + worldXAngle + 'deg) rotateY( ' + worldYAngle + 'deg)';
+  world.style.webkitTransform = t;
+  world.style.MozTransform = t;
+  world.style.oTransform = t;
 
 }
+
+function update (){
+
+  for( var j = 0; j < layers.length; j++ ) {
+    var layer = layers[ j ];
+    layer.data.a += layer.data.speed;
+    var t = 'translateX( ' + layer.data.x + 'px ) translateY( ' + layer.data.y + 'px ) translateZ( ' + layer.data.z + 'px ) rotateY( ' + ( - worldYAngle ) + 'deg ) rotateX( ' + ( - worldXAngle ) + 'deg ) scale( ' + layer.data.s + ')';
+    layer.style.webkitTransform = t;
+    layer.style.MozTransform = t;
+    layer.style.oTransform = t;
+  }
+
+  requestAnimationFrame( update );
+
+}
+
+update();
